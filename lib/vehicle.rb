6128ceae47e59c0e830236cc6d8df3fb37c7248e
Warren Harper II <warrenharper2@att.net>
2019-08-29 22:42:07 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,9 @@ class Vehicle
       @wheel_size = wheel_size
       @wheel_number = wheel_number
     end
-
-
+    def go
+      "vrrrrrrrooom!"
+    end
 
     def fill_up_tank
       "filling up!"
